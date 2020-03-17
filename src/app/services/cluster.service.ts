a9553ed9bbc0f06f47eaa72efb47f292f94d7e46
Alfredo Pedraza <alfpedraza@hotmail.com>
2020-03-17 07:13:51 +0000

    Update the cluster endpoint service

--- src/app/services/cluster.service.ts
+++ src/app/services/cluster.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpHelper } from 'src/app/services/http-helper.service';
 
 /**
- * Consumes a REST endpoint to GET information about the machines
- * in the Kubernetes cluster.
+ * Consumes a REST endpoint to GET information about the nodes
+ * running in the Kubernetes cluster.
  */
 @Injectable({ providedIn: "root"})
 export class ClusterService {
@@ -16,16 +16,16 @@ export class ClusterService {
     public constructor(private http: HttpHelper) { }
 
     /**
-     * Gets the number of machines present in the cluster.
+     * Gets the number of nodes present in the Kubernetes cluster.
      * @returns A Promise<number> object representing the number
-     * of machines present in the cluster.
+     * of nodes present in the cluster.
      */
-    public getMachineCount(): Promise<number> {
-        return this.http.get<number>("/api/cluster/machine-count");
+    public getNodeCount(): Promise<number> {
+        return this.http.get<number>("/api/cluster/node-count");
     }
 
     /**
-     * Gets the CPU and memory metrics of the machines present in the cluster.
+     * Gets the CPU and memory metrics of the nodes present in the cluster.
      * @returns A Promise<string> object representing the metrics.
      */
     public getStatus(): Promise<string> {
